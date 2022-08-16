3fdbff62586bd0a1abd0089a569f7e4a2a28e606
DESKTOP-C5S803A\kytcook <kytcook@naver.com>
2022-08-16 15:18:21 +0900

    enquireNotnull_220816

--- dgym/은택/app/client/views/contact/reservation-2.js
+++ dgym/은택/app/client/views/contact/reservation-2.js
@@ -11,40 +11,47 @@ $("#submit").click(function () {
   // document.write(year + "-" + month + "-" + date);
   /* 현재날짜 */
 
-  const info = [
-    (이름 = $("#name").val()),
-    (성별 = $("#gender").val()),
-    (연락처 =
-      $("#phone1").val() + "-" + $("#phone2").val() + "-" + $("#phone3").val()),
-    (프로그램 = $("#program").val()),
-    (등록날짜 = year + "-" + month + "-" + date),
-    (예약날짜 = $("#time").val()) /* =(html)희망날짜 */,
-    (문의유형 = $("#question").val()),
-    (희망강사 = $("#teacher").val()),
-  ];
+  const 이름 = $("#name").val();
+  const 성별 = $("#gender").val();
+  const 연락처 =
+    $("#phone1").val() + "-" + $("#phone2").val() + "-" + $("#phone3").val();
+  const 프로그램 = $("#program").val();
+  const 등록날짜 = year + "-" + month + "-" + date;
+  const 예약날짜 = $("#time").val(); /* =(html)희망날짜 */
+  const 문의유형 = $("#question").val();
+  const 희망강사 = $("#teacher").val();
 
-  if (info !== (null || "")) {
-    db.collection("enquire")
-      .add({
-        이름,
-        성별,
-        연락처,
-        프로그램,
-        등록날짜,
-        예약날짜,
-        문의유형,
-        희망강사,
-      })
-      .then(() => {
-        console.log(info);
-        alert("접수가 완료 되었습니다.");
-        window.location.replace("./reservation.html");
-      })
-      .catch((error) => {
-        console.error("Error removing document: ", error);
-      });
+  const info = {
+    이름 : $("#name").val(),
+    성별 : $("#gender").val(),
+    연락처 : $("#phone1").val() + "-" + $("#phone2").val() + "-" + $("#phone3").val(),
+    프로그램 : $("#program").val(),
+  }
+
+
+  if (
+    !이름 || !성별 || 
+  ) {
+    alert("아 좀 입력좀");
   } else {
-    alert("빈 칸을 채워주세요");
+    // db.collection("enquire")
+    //   .add({
+    //     이름,
+    //     성별,
+    //     연락처,
+    //     프로그램,
+    //     등록날짜,
+    //     예약날짜,
+    //     문의유형,
+    //     희망강사,
+    //   })
+    //   .then(() => {
+    //     alert("접수가 완료 되었습니다.");
+    //     window.location.replace("./reservation.html");
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error removing document: ", error);
+    //   });
   }
 });
 /*===============  추가 End ===============*/
