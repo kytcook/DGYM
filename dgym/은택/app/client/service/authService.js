9f5656d23b39095af32ff8e44b22feb5ead1e387
kytcook <kytcook@naver.com>
2022-08-19 02:52:56 +0900

    synchro_work_0819

--- dgym/은택/app/client/service/authService.js
+++ dgym/은택/app/client/service/authService.js
@@ -1,55 +1,42 @@
-// 이메일 로그인
-function EmailLogin(mem_email, mem_pw) {
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(mem_email, mem_pw)
-    .then((result) => {
-      console.log(result.user);
-      window.location.replace("./index.html"); //?useruid=${result.user.uid}
-      // replace써야 로그인 후 로그인 페이지로 이동안됨. replace가 뒤로가기를 막아준다
-    })
-    .catch((error) => {
-      window.alert("없는 회원입니다. 다시 확인하세요");
-    });
+// 소셜 로그인 및 이메일 로그인
+class AuthService {
+  // 각 소셜 provider 주입
+  constructor() {
+    this.googleProvider = new firebase.auth.GoogleAuthProvider();
+    this.facebookProvider = new firebase.auth.FacebookAuthProvider();
+    this.githubProvider = new firebase.auth.GithubAuthProvider();
+  }
+  // 삼항연산자로 각 소셜 provider 분기
+  Login(providerName) {
+    let authProvider = null;
+    providerName == "Google"
+      ? (authProvider = this.googleProvider)
+      : providerName == "Facebook"
+      ? (authProvider = this.facebookProvider)
+      : (authProvider = this.githubProvider);
+    firebase
+      .auth()
+      .signInWithPopup(authProvider)
+      .then((result) => {
+        window.location.replace("../../../index.html");
+      })
+      .catch((error) => {
+        let errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
+  // 이메일 로그인
+  EmailLogin(mem_email, mem_pw) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(mem_email, mem_pw)
+      .then((result) => {
+        console.log(result.user);
+        window.location.replace("../../../index.html");
+      })
+      .catch((error) => {
+        window.alert("없는 회원입니다. 다시 확인하세요");
+      });
+  }
 }
-// 구글 로그인
-let provider = new firebase.auth.GoogleAuthProvider();
-function GoogleLogin() {
-  console.log("Login Btn Call");
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((res) => {
-      window.location.href = "./index.html";
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-}
-// 페이스북 로그인
-let provider2 = new firebase.auth.FacebookAuthProvider();
-function FacebookLogin() {
-  firebase
-    .auth()
-    .signInWithPopup(provider2)
-    .then((result) => {
-      var credential = result.credential;
-      // The signed-in user info.
-      var user = result.user;
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var accessToken = credential.accessToken;
-      window.location = "./index.html";
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage);
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-    });
-}
-
-export default { EmailLogin, GoogleLogin, FacebookLogin };
+export { AuthService };
